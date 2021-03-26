c3cf0c1ee21133b7b8efdc4002bd97fb947a0cb5
Monica <m_spiritas@mail.fhsu.edu>
2021-03-25 22:49:48 -0500

    First commit

---
+++ view/register.php
@@ -0,0 +1,5 @@
+<form action="." method="get">
+    <label>Name:</label>
+    <input type="text" name="youname" maxlength="50" required><br>
+    <input id="add_type_button" type="submit" class="button blue" value="Register"><br>
+</form>
