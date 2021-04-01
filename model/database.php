b9ef086db34e352e806494783c73c586ceb96d5c
mspiritas <77894776+mspiritas@users.noreply.github.com>
2021-03-31 22:59:22 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,6 +1,6 @@
 <?php
     //local development server connection
-    $dsn = 'mysql:host=vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=zippyusedautos';
+    $dsn = 'mysql:host=vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=dpiofwiklt0wbc6m';
     $username = 'cmhejv54yxrmw9kh';
     $password = 'ggg8gakj5wltu58o';
 
